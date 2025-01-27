1c4dabc1e6d99bb2cee350b781e63ca8aa51bc4e
hitalukder <htalukder@ibm.com>
2025-01-27 14:47:15 -0500

    made embedding model parameterized

--- 5. SAP Generative AI Hub and HANA Cloud Vector Engine/5.1 Q&A RAG/code/ai-core-rag-pipeline/app/src/services/embedding.py
+++ 5. SAP Generative AI Hub and HANA Cloud Vector Engine/5.1 Q&A RAG/code/ai-core-rag-pipeline/app/src/services/embedding.py
@@ -2,15 +2,16 @@
 from dotenv import load_dotenv
 load_dotenv()
 from gen_ai_hub.proxy.native.openai import embeddings
-
+import os
 
 class Embedding:
     def __init__(self) -> None:
         pass
 
-    def get_embedding_gen_ai(self, input, model="text-embedding-ada-002") -> str:
+    def get_embedding_gen_ai(self, input) -> str:
+        text_embedding_model = os.environ.get("EMBEDDING_MODEL", default="text-embedding-3-large")
         response = embeddings.create(
-        model_name=model,
+        model_name=text_embedding_model,
         input=input
         )
         return response.data[0].embedding
